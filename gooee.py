d8a6734868519b23f499833dfd1c99d0321b33d8
Marcell Mars <ki.ber@kom.uni.st>
2016-11-02 05:56:55 +0100

    meh

--- gooee.py
+++ gooee.py
@@ -5,6 +5,9 @@ from __future__ import (unicode_literals, division, absolute_import,
 
 import sys
 import json
+import signal
+import pprint
+pp = pprint.pprint
 
 from PyQt5.Qt import QObject
 from PyQt5.Qt import QApplication
@@ -45,14 +48,13 @@ class CrossClient(QObject, ApplicationSession):
 
 
 class Gooee(QDialog):
-    closed = pyqtSignal()
-
     def __init__(self, url, realm, parent=None):
         QDialog.__init__(self)
 
         self.url = url
         self.realm = realm
         self.session = None
+        self.subscriptions = {}
 
         def make(config):
             self.session = CrossClient(config)
@@ -78,7 +80,7 @@ class Gooee(QDialog):
                                        QSizePolicy.Expanding)
         self.pub_message.setToolTip("Type your message here")
 
-        self.publish_channel = QLineEdit("com.accorder.me")
+        self.publish_channel = QLineEdit("com.accorder.js")
         self.send_message = QPushButton("Publish")
 
         self.send_message.clicked.connect(
@@ -99,12 +101,12 @@ class Gooee(QDialog):
 
         self.subscribe_label = QLabel("Subscribe: ")
 
-        self.sub_callback = QLineEdit("callback")
+        self.sub_callback = QLineEdit("self.on_js_message")
         self.sub_callback.setObjectName("callback")
         self.sub_callback.setSizePolicy(QSizePolicy.Expanding,
                                         QSizePolicy.Expanding)
 
-        self.subscribe_channel = QLineEdit("com.accorder.me")
+        self.subscribe_channel = QLineEdit("com.accorder.js")
         self.subscribe = QPushButton("Subscribe")
 
         self.subscribe.clicked.connect(
@@ -143,38 +145,49 @@ class Gooee(QDialog):
             print("subscriptions: {}".format(s))
 
     def on_join_session(self):
-        print("Session joined: {}".format(self.session.__dict__))
+        pprint.pprint("session.id: {}".format(self.session._subscriptions))
 
     def on_leave_session(self):
         print('leave')
 
     def on_python_message(self, message):
+        print("session.id: {}".format(self.session))
         self.py_recv.setText("From py: {}".format(message))
         print("on_python: {}".format(message))
+        for s in self.session._subscriptions:
+            print("subscriptions (via python): {}".print(s))
 
     def on_js_message(self, message):
         self.js_recv.setText("From js: {}".format(message))
+        pprint.pprint("session.id: {}".format(dict(self.session._subscriptions)))
+        session_dict = dict(self.session._subscriptions)
+        pp(session_dict)
+        for sub_id,i in session_dict.items():
+            pp(sub_id)
+            pp(i[0].topic)
+            # pp('sub_id: {}\ntopic: {}'.format(sub_id, i['topic']))
+
         print("on_js: {}".format(message))
-        for s in self.session._subscriptions:
-            print("subscriptions (via js): {}".print(s))
+
+    def closeEvent(self,ev):
+        if reactor.threadpool is not None:
+            reactor.threadpool.stop()
+        else:
+            reactor.stop
+        app.quit()
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        app = QApplication(sys.argv)
-        app.setApplicationName("Gooee")
-        qt5reactor.install()
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+        app = QApplication(sys.argv)
+        app.setApplicationName("gooee")
+
+        qt5reactor.install()
         from twisted.internet import reactor
 
-        def quit_app():
-            reactor.addSystemEventTrigger('before', 'shutdown', callable)
-            if reactor.threadpool is not None:
-                reactor.threadpool.stop()
-            app.quit()
-
         main = Gooee(url=u"ws://127.0.0.1:8080/ws", realm="realm1")
-        app.aboutToQuit.connect(quit_app)
         main.show()
 
         reactor.run()
